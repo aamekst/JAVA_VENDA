f7320e699bbef740df9479d8d2384f58ef5e57f8
aamekst <mieko.alana@gmail.com>
2024-11-04 20:26:12 -0300

    'cross_venda'

--- src/main/java/com/api_vendinha/api/controller/VendaController.java
+++ src/main/java/com/api_vendinha/api/controller/VendaController.java
@@ -13,7 +13,7 @@ import org.springframework.web.context.request.WebRequest;
 
 import java.util.List;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/vendas")
 public class VendaController {
